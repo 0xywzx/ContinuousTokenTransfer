80c182ef38641aaded36c455e859ce6bead46f60
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-05-27 16:12:07 +0900

    transfer function

--- contracts/ContinuousTokenTransfer.sol
+++ contracts/ContinuousTokenTransfer.sol
@@ -1,11 +1,16 @@
 pragma solidity >=0.4.21 <0.7.0;
 
+import "../openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 
 contract ContinuousTokenTransfer {
   address public owner;
 
   constructor() public {
     owner = msg.sender;
+  }
+
+  function ContinuousTokenTransfe(address _tokeAddress, address _to, uint256 _value) public returns (bool success) {
+    IERC20(_tokeAddress).transfer(_to, _value);
   }
 
 }
