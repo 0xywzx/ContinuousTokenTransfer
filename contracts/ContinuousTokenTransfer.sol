8b345a02d3b3d427e9e82595694cadd4b84fa168
Yosuke-Aramaki <gnx.vw903@gmail.com>
2020-05-27 15:49:32 +0900

    installed openzeppelin

--- contracts/ContinuousTokenTransfer.sol
+++ contracts/ContinuousTokenTransfer.sol
@@ -1,18 +1,11 @@
 pragma solidity >=0.4.21 <0.7.0;
 
+
 contract ContinuousTokenTransfer {
   address public owner;
-  uint public last_completed_migration;
 
   constructor() public {
     owner = msg.sender;
   }
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
-
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
 }
